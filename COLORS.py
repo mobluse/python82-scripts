fbb55cc3e9942d031564b8584905160b19368ad9
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-04 00:12:10 +0200

    Update COLORS.py

--- COLORS.py
+++ COLORS.py
@@ -1,9 +1,9 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
-print("\033[0;H",end="")
+print(end="\033[0;H")
 i=32
 for bg in range(16):
   for fg in range(16):
-    print("\033[%d;%dm%c"%(fg,bg,i),end="")
+    print(end="\033[%d;%dm%c"%(fg,bg,i))
     i+=1
     if i>127:
       i=32
@@ -12,4 +12,4 @@ while True:
   a=input("\033[m\033[8;HEsc code? \\033\033[K")
   if a=="-1":
     break
-  print("\033[5;24HX\033%s"%a,end="")
+  print(end="\033[5;24HX\033%s"%a)
