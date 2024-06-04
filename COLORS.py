5029a1dc531de86076bf8900f9fb154c1864ade1
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-04 21:35:38 +0200

    Update COLORS.py

--- COLORS.py
+++ COLORS.py
@@ -1,5 +1,5 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
-def move(r,c):
+def at(r,c):
   return "\x1B[%d;%dH"%(r,c)
 
 def co(fg,bg):
@@ -8,7 +8,7 @@ def co(fg,bg):
 def cls():
   return "\x1B[2J"
 
-def clln():
+def cll():
   return "\x1B[K"
 
 def prco(fg,bg):
@@ -27,7 +27,7 @@ for bg in range(0,16):
   for fg in range(0,16):
     prco(fg,bg)
 while True:
-  a=input("%s%sEsc code? \\033%s"%(co(0,7),move(8,0),clln()))
+  a=input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))
   if a=="-1":
     break
-  print(end="%sX\x1B%s"%(move(5,24),a))
+  print(end="%sX\x1B%s"%(at(5,24),a))
