8db0ab11427e6ff529d5653f8c4a58a743bd1427
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-07 21:07:51 +0200

    Update COLORS.py

--- COLORS.py
+++ COLORS.py
@@ -1,9 +1,9 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
-def at(r,c):
-  return "\x1B[%d;%dH"%(r,c)
-
 def co(fg,bg):
   return "\x1B[%d;%dm"%(fg,bg)
+
+def at(r,c):
+  return "\x1B[%d;%dH"%(r,c)
 
 def cls():
   return "\x1B[2J"
