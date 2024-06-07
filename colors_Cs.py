5d6cb2e6583660a405a7f4ab166471bfa2c2dde4
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-07 21:05:53 +0200

    Update and rename colors_dt.py to colors_Cs.py

--- colors_Cs.py
+++ colors_Cs.py
@@ -1,13 +1,15 @@
+# python3 colors_Cs.py
+# snek colors_Cs.py
 fgco=[30,91,32,93,94,95,96,97,92,37,90,31,33,35,34,36]
 bgco=fgco[:]
 for i in range(len(bgco)):
   bgco[i]+=10
 
-def at(r,c):
-  return "\x1B[%d;%dH"%(r+1,c+1)
-
 def co(fg,bg):
   return "\x1B[%d;%dm"%(fgco[fg],bgco[bg])
+
+def at(r,c):
+  return "\x1B[%d;%dH"%(r+1,c+1)
 
 def cls():
   return at(0,0)+"\x1B[2J"
@@ -30,7 +32,7 @@ i=32
 for bg in range(0,16):
   for fg in range(0,16):
     prco(fg,bg)
-#  if bg%2: print()
+  if bg%2: print()
 while True:
   a=input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))
   if a=="-1":
