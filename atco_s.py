407446c31f2bd68f64d8dc2f9ce0c3bd63c97649
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-11 01:58:37 +0200

    Create atco_s.py

---
+++ atco_s.py
@@ -0,0 +1,20 @@
+# 
+fgco=[30,91,32,93,94,95,96,97,92,37,90,31,33,35,34,36]
+bgco=fgco[:]
+for i in range(len(bgco)):
+  bgco[i]+=10
+
+def co(fg,bg):
+  return "\x1B[%d;%dm"%(fgco[fg],bgco[bg])
+
+def at(r,c):
+  return "\x1B[%d;%dH"%(r+1,c+1)
+
+def cls():
+  return at(0,0)+"\x1B[2J"
+
+def shcu(on):
+  if on:
+    return "\x1B[?25h"
+  else:
+    return "\x1B[?25l"
