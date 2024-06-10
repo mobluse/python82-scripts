d6568dab939521696910eb8390ebbb529da3b865
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-11 01:57:01 +0200

    Update flappbat_s.py

--- flappbat_s.py
+++ flappbat_s.py
@@ -2,26 +2,7 @@
 # Run using: snek flappbat_s.py
 import time
 import random
-
-fgco=[30,91,32,93,94,95,96,97,92,37,90,31,33,35,34,36]
-bgco=fgco[:]
-for i in range(len(bgco)):
-  bgco[i]+=10
-
-def co(fg,bg):
-  return "\x1B[%d;%dm"%(fgco[fg],bgco[bg])
-
-def at(r,c):
-  return "\x1B[%d;%dH"%(r+1,c+1)
-
-def cls():
-  return at(0,0)+"\x1B[2J"
-
-def shcu(on):
-  if on:
-    return "\x1B[?25h"
-  else:
-    return "\x1B[?25l"
+from atco_s import *
 
 print(end=shcu(False))
 print(end="%s%s"%(co(8,0),cls()))
