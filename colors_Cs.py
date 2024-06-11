ee095913ce976a595f9c1a7867db30d2e8ccaec4
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-11 02:01:02 +0200

    Update colors_Cs.py

--- colors_Cs.py
+++ colors_Cs.py
@@ -1,22 +1,7 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
 # python3 colors_Cs.py
 # snek colors_Cs.py
-fgco=[30,91,32,93,94,95,96,97,92,37,90,31,33,35,34,36]
-bgco=fgco[:]
-for i in range(len(bgco)):
-  bgco[i]+=10
-
-def co(fg,bg):
-  return "\x1B[%d;%dm"%(fgco[fg],bgco[bg])
-
-def at(r,c):
-  return "\x1B[%d;%dH"%(r+1,c+1)
-
-def cls():
-  return at(0,0)+"\x1B[2J"
-
-def cll():
-  return "\x1B[K"
+from atco_s import *
 
 def prco(fg,bg):
   global i
