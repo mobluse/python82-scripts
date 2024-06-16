5d76dbe3340259df57b39b38ba57e2138e5efc2c
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-16 20:10:49 +0200

    Update colors_C.py

--- colors_C.py
+++ colors_C.py
@@ -1,5 +1,5 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
-# python3 colors_Cs.py
+# python3 colors_C.py
 from atco_s import *
 
 def prco(fg,bg):
@@ -12,14 +12,14 @@ def prco(fg,bg):
   if i>127:
     i=32
 
-print(end="%s%s"%(co(0,7),cls()))
+print(end=co(0,7)+cls())
 i=32
 for bg in range(0,16):
   for fg in range(0,16):
     prco(fg,bg)
   if bg%2: print()
 while True:
-  a=input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))
+  a=eval('"'+input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))+'"')
   if a=="-1":
     break
   print(end="%sX\x1B%s"%(at(5,24),a))
