24caa3d5eb2d8962fc779b5c14ecee4d0f17c88a
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-17 00:05:16 +0200

    Update ION.py

--- ION.py
+++ ION.py
@@ -1,4 +1,5 @@
 # Not by me and doesn't work now, but worked with an earlier version of the Python82 App
+# Found on Jun 17 2024 https://tiplanet.org/forum/viewtopic.php?f=41&t=24977
 from sys import *
 
 def getkey():
