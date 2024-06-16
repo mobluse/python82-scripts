947ca571e13253aa562c5733b58c51884bf1c14d
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-06-16 20:13:46 +0200

    Update COLORS.py

--- COLORS.py
+++ COLORS.py
@@ -1,5 +1,5 @@
 # Colors (C) 2024 by Mikael O. Bonnier, Lund, Sweden. License: GPLv3+.
-from atco import *
+from ATCO import *
 
 def prco(fg,bg):
   global i
@@ -11,13 +11,13 @@ def prco(fg,bg):
   if i>127:
     i=32
 
-print(end="%s%s"%(co(0,7),cls()))
+print(end=co(0,7)+cls())
 i=32
 for bg in range(0,16):
   for fg in range(0,16):
     prco(fg,bg)
 while True:
-  a=input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))
+  a=eval('"'+input("%s%sEsc code? \\033%s"%(co(0,7),at(8,0),cll()))+'"')
   if a=="-1":
     break
   print(end="%sX\x1B%s"%(at(5,24),a))
